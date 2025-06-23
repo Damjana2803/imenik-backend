2484af845469a21b217eef4f719fec855ba8d957
djordje41 <djordje.zubac@codebears.io>
2025-06-23 17:51:09 +0200

    Fix cors 3

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,25 @@
 <?php
 
 return [
+
+    // let the middleware run on absolutely every request
     'paths' => ['*'],
 
+    // allow any HTTP verb the browser might pre-flight
     'allowed_methods' => ['*'],
-    'allowed_headers' => ['*'],
 
+    // wildcard origin is OK because we are NOT sending cookies
     'allowed_origins' => ['*'],
 
+    // no regex needed when ^^^ is '*'
     'allowed_origins_patterns' => [],
 
+    // Accept the headers your SPA will actually send
+    'allowed_headers' => ['*'],
+
+    // we are NOT using cookies -> KEEP THIS FALSE
     'supports_credentials' => false,
 
     'exposed_headers' => [],
-    'max_age' => 0,
+    'max_age'         => 0,
 ];
